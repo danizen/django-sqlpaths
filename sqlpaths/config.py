a0180251771e3215f3c1a3565b7d065e9a25b5b8
Dan Davis <dan@danizen.net>
2019-09-20 00:07:13 -0400

    Setup structure of application

--- sqlpaths/config.py
+++ sqlpaths/config.py
@@ -2,6 +2,13 @@ import attr
 
 
 @attr.s(frozen=True)
-class SqlPathsConfig():
-    pass
+class PathsConfig():
+    DBSHELL = attr.ib(kw_only=True, default=False)
+    INCLUDE = attr.ib(kw_only=True, default=['*.sql'])
+    ROOT = attr.ib(kw_only=True, default=None)
+    ALWAYS = attr.ib(kw_only=True, default=[])
+
+    def __attrs_post_init__(self):
+    	if not self.ROOT:
+    		self.ROOT = os.path.join(settings.BASE_DIR, 'sql')
 
